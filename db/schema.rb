cda2d3697d1ac03450d2841de571ac8432277788
fanfannb <qingfengbf01@gmail.com>
2022-06-03 16:08:46 +0800

    feat: add creator to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_03_061039) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_03_070305) do
   create_table "tasks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_03_061039) do
     t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id"
+    t.index ["creator_id"], name: "index_tasks_on_creator_id"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
